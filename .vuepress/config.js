9c6203e1766ee761830f4f52b6d726f6513e04a1
joeappleton18 <joe.appleton@solent.ac.uk>
2020-10-05 17:15:03 +0100

    deploy first week

---
+++ .vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  base: "/web-dev-2020-notes/",
+  dest: "docs",
+  themeConfig: {
+    displayAllHeaders: true,
+    sidebar: [
+      {
+        title: "Overview",
+        collapsable: true,
+        children: ["/"],
+      },
+
+      {
+        title: "Week 1 - The latest version of JavaScript",
+        collapsable: true,
+        children: ["/sessions/week_1/"],
+      },
+    ],
+  },
+};
